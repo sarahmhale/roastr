acf09564f78052b251d759587a3ebbf9f4acf3f7
Sarah <sarahhale95@gmail.com>
2020-11-14 23:37:08 +0100

    svelte and jest test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  transform: {
-    '^.+\\.svelte$': 'svelte-jester',
-    '^.+\\.js$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'svelte'],
-}
+      "transform": {
+      "^.+\\.svelte$": [
+        "svelte-jester",
+        {
+          "preprocess": true
+        }
+      ],
+      "^.+\\.ts$": "ts-jest",
+      "^.+\\.js$": "babel-jest"
+    },
+    "moduleFileExtensions": [
+      "js",
+      "ts",
+      "svelte"
+    ]
+};
