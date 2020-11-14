c37d34a00db6c0a6738ce6d55f4725651a68cc82
Sofia Tovedal <sofia.tovedal@gmail.com>
2020-11-14 23:46:15 +0100

    added folders for components and stores

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
-	let name = 'world';
-</script>
 
-<h1>Hello {name}!</h1>
+import Header from './components/Header/index.svelte'
+import { reviews } from './stores/reviews'
+</script>
+<style>	
+:global(body) {
+	margin: 0;
+	padding: 0;
+}
+</style>
+<div id="main">
+	<Header />
+	<p>Reviews:{$reviews}</p>
+</div>
+
---
+++ src/components/Header/index.svelte
@@ -0,0 +1,7 @@
+<style>
+	#header {
+		background-color: lightgray;
+		padding: 1rem;
+	}
+</style>
+<div id="header">ROASTR</div>
